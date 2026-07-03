a4eec0acd5ce6a69b6c6b780c3bb8e534a27c149
Pragna-G12 <pragna122004@gmail.com>
2026-07-03 23:34:39 +0530

    Create checker.py

---
+++ checker.py
@@ -0,0 +1,11 @@
+Purpose: Contains only the password validation logic.
+
+It should contain:
+
+Length check
+Uppercase check
+Lowercase check
+Number check
+Special character check
+Strength calculation
+Return result
